db4fc12669ed7e134684e3395de21a3be630d147
Emily Franz-Lien <emilyfranz-lien@Emilys-Air.gateway.mts.net>
2017-04-22 19:43:55 -0500

    Initial commit

---
+++ FantasticView/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  FantasticView
+//
+//  Created by Emily Franz-Lien on 2017-04-22.
+//  Copyright © 2017 Emily Franz-Lien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fantasticView = FantasticView(frame: self.view.bounds)
+        
+        self.view.addSubview(fantasticView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
